c6aa082e4975691d8a99a2674ab65077b4177819
Moises Baltazar <me@moisesb.com>
2020-04-30 13:16:05 -0500

    Add docstring to the generic functions of Loader

--- Crosswalk/Loader.py
+++ Crosswalk/Loader.py
@@ -9,34 +9,52 @@ class Loader:
         self.setup()
 
     def setup(self):
-        # load datadictionaries or any other file that will aid in interpreting fields
-        # for creating shadow dataframe later on
+        """
+        Use this function to load data dictionaries or any other file that will
+        aid in interpreting fields for creating shadow dataframes later on.
+        """
         pass
 
     def get_source_name(self):
         return self.name
 
     def _fields_hook_(self, fields):
-        # add any special fields that are not explicity stated in yaml map files
+        """
+        Use this function to add sepcial fields that are not explicitly
+        described in the yaml map files.
+        """
         return fields
 
     def _load_hook_(self, fields):
-        # Implement actual loading of data
+        """
+        Implements the actual loading of the data.
+        """
         return pd.DataFrame()
 
     def _post_load_hook_(self, df):
-        # Merge additional data sources or manipulate the dataframe to create additional fields
+        """
+        Manipulate the dataframe to create additional or rename existing fields.
+        This is also a good place to merge additional data.
+        """
         return df
 
     def _detect_missing_fields_hook_(self, df, fields):
-        # From the fields requested, which fields are missing/unavailable
+        """
+        Notify the user if any of the requested fields are missing or unavailable.
+        """
         return fields
 
     def _create_shadow_dataframe(df, fields):
-        # Create a shadow dataframe with alternatives for field (like text instead of internal code)
+        """
+        Create a shadow dataframe which for example contains the text
+        labels for coded fields.
+        """
         return df.copy(), df.copy()
 
     def load(self, fields):
+        """
+        The main function that runs through all of the above functions in order.
+        """
         fields = self._fields_hook_(fields)
         df = self._load_hook_(fields)
         df = self._post_load_hook_(df)

348e5705689158d79a00a51c293724b6ab86311f
Moises Baltazar <me@moisesb.com>
2020-05-06 07:44:47 -0500

    Add timing to DataCache preload method

--- Crosswalk/DataCache.py
+++ Crosswalk/DataCache.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import time
 
 class DataCache:
     def __init__(self, *sources):
@@ -17,9 +18,11 @@ class DataCache:
             d[source].add(field)
 
         for source, fields in d.items():
+            start = time.time()
             df, DF = self.sources[source].load(list(fields))
             self.df[source] = df
             self.DF[source] = DF
+            print("Timing: ", source, (time.time()-start))
 
     def get(self, source, field):
         if field in self.df[source]:
